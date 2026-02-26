0972aa204869389c626daa3b7cd13f8b618a488b
Aleksandar Angelov <deirror.dev@gmail.com>
2026-02-26 22:11:36 +0200

    feat: add transport impls

---
+++ transport/http/transport.go
@@ -0,0 +1,35 @@
+package httpx
+
+import (
+	"io"
+
+	"github.com/Deirror/servette/transport/http/client"
+)
+
+type Transport struct {
+	cl *client.Client
+}
+
+func NewTransport(cl *client.Client) *Transport {
+	return &Transport{
+		cl: cl,
+	}
+}
+
+// Send implements Transport.Send (POST/PUT/any payload)
+func (u *Transport) Send(path string, contentType string, body io.Reader) (io.ReadCloser, error) {
+	resp, err := u.cl.Post(path, contentType, body)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
+}
+
+// Get implements Transport.Get
+func (u *Transport) Get(path string) (io.ReadCloser, error) {
+	resp, err := u.cl.Get(path)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
+}

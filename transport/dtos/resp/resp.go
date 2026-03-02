338bca4bc9c6c1449f59291f66dc49887aa8fdd5
Aleksandar Angelov <deirror.dev@gmail.com>
2026-03-02 17:37:19 +0200

    feat: add dto resp

---
+++ transport/dtos/resp/resp.go
@@ -0,0 +1,22 @@
+package respx
+
+type Resp struct {
+	Code    string `json:"code"`
+	MsgKey  string `json:"message_key"`
+	Payload any    `json:"payload,omitempty"`
+}
+
+func New(code, key string) *Resp {
+	return &Resp{
+		Code:   code,
+		MsgKey: key,
+	}
+}
+
+func NewWithPayload(code, key string, payload any) *Resp {
+	return &Resp{
+		Code:    code,
+		MsgKey:  key,
+		Payload: payload,
+	}
+}

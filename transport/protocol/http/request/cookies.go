968ae62e50250f31b224967658ad5d59927ac2fa
Aleksandar Angelov <deirror.dev@gmail.com>
2026-03-05 15:25:24 +0200

    feat: add cookie func wrapper

---
+++ transport/protocol/http/request/cookies.go
@@ -0,0 +1,23 @@
+package request
+
+import (
+	"errors"
+	"net/http"
+)
+
+func Cookies(r *http.Request, keys ...string) ([]*http.Cookie, error) {
+	if len(keys) < 1 {
+		return nil, errors.New("empty keys slice")
+	}
+
+	var cookies []*http.Cookie
+	for _, key := range keys {
+		val, err := r.Cookie(key)
+		if err != nil {
+			return nil, err
+		}
+		cookies = append(cookies, val)
+	}
+
+	return cookies, nil
+}

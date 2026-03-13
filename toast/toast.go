8cee004cc67303736b2651041608a4144b7e1f7e
Deirror <deirror.dev@gmail.com>
2026-03-13 17:34:41 +0200

    feat: add toast write funcs

---
+++ toast/toast.go
@@ -0,0 +1,42 @@
+package toast
+
+import (
+	"net/http"
+
+	"github.com/Deirror/servette/encoding/json"
+	"github.com/Deirror/servette/transport/protocol/http/header"
+)
+
+type payload struct {
+	Message string `json:"message"`
+	Type    string `json:"type"`
+}
+
+func trigger(w http.ResponseWriter, msg string, t string) {
+	data := map[string]payload{
+		"showToast": {
+			Message: msg,
+			Type:    t,
+		},
+	}
+
+	b, _ := json.Marshal(data)
+
+	w.Header().Set(header.HXTriggerAfterSwap, string(b))
+}
+
+func Success(w http.ResponseWriter, msg string) {
+	trigger(w, msg, "success")
+}
+
+func Error(w http.ResponseWriter, msg string) {
+	trigger(w, msg, "error")
+}
+
+func Info(w http.ResponseWriter, msg string) {
+	trigger(w, msg, "info")
+}
+
+func Warning(w http.ResponseWriter, msg string) {
+	trigger(w, msg, "warning")
+}

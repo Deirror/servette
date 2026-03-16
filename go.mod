4a2d5d4ec3da382aad634fdce2fe4e0c13cf1e36
Aleksandar Angelov <deirror.dev@gmail.com>
2026-03-16 20:09:50 +0200

    feat: add templ render wrapper func

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 require github.com/golang-jwt/jwt/v5 v5.3.1
 
 require (
+	github.com/a-h/templ v0.3.1001 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-chi/chi/v5 v5.2.5 // indirect
@@ -25,7 +26,7 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.5.0
-	golang.org/x/sync v0.15.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 )
 
---
+++ transport/protocol/http/templ/templ.go
@@ -0,0 +1,29 @@
+package templ
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+
+	"github.com/a-h/templ"
+
+	"github.com/Deirror/servette/transport/protocol/http/header"
+	"github.com/Deirror/servette/transport"
+	"github.com/Deirror/servette/transport/err"
+	httperr "github.com/Deirror/servette/transport/protocol/http/err"
+)
+
+func Render(ctx context.Context, c templ.Component, w http.ResponseWriter) *errx.Err {
+	// Render page into a buffer
+	var buf bytes.Buffer
+	if err := c.Render(ctx, &buf); err != nil {
+		return httperr.New(http.StatusInternalServerError, transport.TemplFail, "cannot render template", err)
+	}
+
+	// Success - write headers + body
+	w.Header().Set(header.ContentType, header.TextHTML)
+	w.WriteHeader(http.StatusOK)
+	buf.WriteTo(w)
+
+	return nil
+}

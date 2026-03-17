a243bd41604e10598d7ae2dcd848dbd789322746
Aleksandar Angelov <deirror.dev@gmail.com>
2026-03-17 19:34:44 +0200

    feat: add hx req wrapper with err

--- transport/protocol/http/htmx/utils.go
+++ transport/protocol/http/htmx/utils.go
@@ -5,8 +5,19 @@ package htmx
 
 import (
 	"net/http"
+
+	"github.com/Deirror/servette/transport"
+	"github.com/Deirror/servette/transport/err"
+	httperr "github.com/Deirror/servette/transport/protocol/http/err"
 )
 
 func IsHXRequest(r *http.Request) bool {
 	return r.Header.Get(HXRequest) == "true"
+}
+
+func RequireHXRequest(r *http.Request) *errx.Err {
+	if !IsHXRequest(r) {
+		return httperr.New(http.StatusBadRequest, transport.RequestBad, "cannot process non-hx requests", nil)
+	}
+	return nil
 }

dbb0e94541922bbd1eedc076eb74f5486c8e8886
Aleksandar Angelov <deirror.dev@gmail.com>
2026-03-28 20:38:32 +0200

    fix: add x to package

--- platform/config/env/init/config.go
+++ platform/config/env/init/config.go
@@ -1,7 +1,7 @@
 // Copyright 2025 Deirror. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-package init
+package initx
 
 import (
 	"github.com/Deirror/servette/config/env/config"

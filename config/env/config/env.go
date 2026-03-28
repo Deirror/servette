418ffe479f1e88831cb5954b54ba8cf4a3f47ea8
Aleksandar Angelov <deirror.dev@gmail.com>
2026-03-28 20:29:11 +0200

    feat: add reusbale init cfg

--- config/env/config/env.go
+++ config/env/config/env.go
@@ -1,7 +1,7 @@
 // Copyright 2025 Deirror. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-package env
+package config
 
 import (
 	"github.com/Deirror/servette/config"
---
+++ platform/config/env/init/config.go
@@ -0,0 +1,36 @@
+// Copyright 2025 Deirror. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+package init
+
+import (
+	"github.com/Deirror/servette/config/env/config"
+	"github.com/Deirror/servette/env"
+)
+
+// Config is a special config which has the app paths for
+// the actual configs to be loaded.
+type Config struct {
+	Cfgs config.MultiConfig
+}
+
+func NewConfig(cs config.MultiConfig) *Config {
+	return &Config{
+		Cfgs: cs,
+	}
+}
+
+// LoadConfig loads OS env vars and gets app paths.
+// It is used to identify how your app should load the actual configs.
+func LoadConfig() (*Config, error) {
+	if err := env.Load(); err != nil {
+		return nil, err
+	}
+
+	cfgs, err := config.LoadMultiConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	return NewConfig(cfgs), nil
+}
